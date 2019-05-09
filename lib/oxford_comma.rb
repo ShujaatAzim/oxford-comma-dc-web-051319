8431ddb61b0ce4cb0fa852cf62e10a87a0729793
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 02:42:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join()
+  elsif array.length == 2
+    return array[0] + " and " + array[1]
   else
   popped = array.pop()
   joined = array.join(", ")
